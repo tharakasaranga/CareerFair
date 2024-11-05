eb20fef27818ebaac30e123bd3db9fbf63c786b9
K.A. Tharaka Saranga <152644410+tharakasaranga@users.noreply.github.com>
2024-11-05 20:06:30 +0530

    Delete backEnd directory

--- backEnd/db_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('db_connection.php'); 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
